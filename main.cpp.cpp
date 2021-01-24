2e253b7f625987aecbcd0c79c8fec048d83a58ef
coder-ot-boga <57289892+coder-ot-boga@users.noreply.github.com>
2021-01-24 21:15:57 +0300

    Add files via upload

---
+++ main.cpp.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	const int size = 10;
+	int arr[size]{};
+	bool alreadythere;
+	for (int i = 0; i < size;)
+	{
+		alreadythere = false;
+		int newgenerate = rand() % 20;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == newgenerate)
+			{
+				alreadythere = true;
+				break;
+			}
+		}
+		if (alreadythere != true)
+		{
+			arr[i] = newgenerate;
+			i++;
+		}
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
