2df62f432c71c4843196a3d61995bce6f00514ce
coder-ot-boga <57289892+coder-ot-boga@users.noreply.github.com>
2021-01-26 23:45:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 using namespace std;
 
 int main()
 {
 	srand(time(NULL));
-	const int size = 10;
-	int arr[size]{};
+	const int SIZE = 10;
+	int arr[SIZE]{};
 	bool alreadythere;
-	for (int i = 0; i < size;)
+	for (int i = 0; i < SIZE;)
 	{
 		alreadythere = false;
 		int newgenerate = rand() % 20;
+
 		for (int j = 0; j < i; j++)
 		{
 			if (arr[j] == newgenerate)
@@ -25,9 +26,24 @@ int main()
 			arr[i] = newgenerate;
 			i++;
 		}
+
 	}
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << endl;
 	}
+
+	cout << endl;
+	cout << sizeof(arr) << " - Array size from " << SIZE << " values!\n";
+	cout << sizeof(arr) / sizeof(int) << " - The total number of values in the array\n";
+
+	int min_v = arr[0];
+	for (int s = 1; s < SIZE; s++)
+	{
+		if (arr[s] < min_v)
+		{
+			min_v = arr[s];
+		}
+	}
+	cout << min_v << " - min value array!\n";
 }
